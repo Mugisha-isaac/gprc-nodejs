01eb19b9f93ab64f227c4eaeb583d3ee40044d88
Mugisha-isaac <mugishaisaac2001@gmail.com>
2022-09-13 22:41:34 +0200

    [feat: client.js] implementing services

--- client.js
+++ client.js
@@ -5,5 +5,30 @@ const bookStorePackage = grpc.loadPackageDefinition(packageDefinition).bookStore
 
 const client = new bookStorePackage.Book('localhost:50051', grpc.credentials.createInsecure());
 client.createBook({'id':-1, 'book': 'Cracking the interview'}, (err,response)=>{
-    
+    if(err){
+        console.log(err);
+    }
+    else{
+        console.log(`From server`, JSON.stringify(response));
+    }
+});
+
+
+client.readBook({'id': 1}, (err,response)=>{
+    if(err){
+        console.log(err);
+    }
+    else{
+        console.log(`From server `, JSON.stringify(response));
+    }
+});
+
+
+client.readBooks(null,(err,response)=>{
+       if(err){
+        console.log(err);
+       }
+       else{
+          console.log(`From server `, JSON.stringify(response));
+       }
 })
